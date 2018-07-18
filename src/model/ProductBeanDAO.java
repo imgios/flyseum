992e98507a5d0f70c2df33df569962d2182e4813
imgios <sulipano.gs@gmail.com>
2018-07-18 10:20:29 +0200

    Minor fixes

--- src/model/ProductBeanDAO.java
+++ src/model/ProductBeanDAO.java
@@ -15,7 +15,7 @@ public class ProductBeanDAO {
 
 			conn = ConnectionPool.getConnection();
 
-			String sqlInsert = ("insert into flight (departure, destination, company, dateDeparture, dateArrival) values (?, ?, ?, ?, ?);");
+			String sqlInsert = ("insert into flight (departure, destination, company, dateDeparture, dateArrival, price) values (?, ?, ?, ?, ?, ?);");
 
 			prepstat = conn.prepareStatement(sqlInsert);
 			prepstat.setString(1, partenza);
@@ -25,17 +25,19 @@ public class ProductBeanDAO {
 			//prepstat.setTimestamp(5, new java.sql.Timestamp(dataArrivo.getTime()));
 			prepstat.setTimestamp(4, dataPartenza);
 			prepstat.setTimestamp(5, dataArrivo);
+			prepstat.setFloat(6, prezzo);
 			int state = prepstat.executeUpdate();
 
 			if (state != 0) {
 				try {
-					String sqlRetrieve = ("SELECT * FROM flight WHERE departure = ? AND destination = ? AND company = ? AND dateDeparture = ? AND dateArrival = ?");
+					String sqlRetrieve = ("SELECT * FROM flight WHERE departure = ? AND destination = ? AND company = ? AND dateDeparture = ? AND dateArrival = ? AND price = ?");
 					PreparedStatement stat = conn.prepareStatement(sqlRetrieve);
 					stat.setString(1, partenza);
 					stat.setString(2, destinazione);
 					stat.setString(3, compagnia);
 					stat.setTimestamp(4, new java.sql.Timestamp(dataPartenza.getTime()));
 					stat.setTimestamp(5, new java.sql.Timestamp(dataArrivo.getTime()));
+					stat.setFloat(6, prezzo);
 					ResultSet rs = stat.executeQuery();
 					rs.next();
 					ProductBean pb = new ProductBean(rs.getInt("id"), rs.getString("departure"), rs.getString("destination"), rs.getString("company"), rs.getFloat("price"), rs.getTimestamp("dateDeparture"), rs.getTimestamp("dateArrival"), rs.getInt("passengers"));
