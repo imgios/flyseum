6a7918330ccfef8bcd1b0c2933ec1ae43b2dd9cb
imgios <sulipano.gs@gmail.com>
2018-07-19 12:25:49 +0200

    New servlet that adds flight inside the cart

---
+++ src/controller/SelectFlight.java
@@ -0,0 +1,60 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.CartBean;
+import model.ProductBean;
+import model.ProductBeanDAO;
+
+/**
+ * Servlet implementation class SelectFlight
+ */
+@WebServlet("/selectFlight")
+public class SelectFlight extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SelectFlight() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		HttpSession session = request.getSession();
+		
+		Integer flightId = Integer.parseInt(request.getParameter("flightId"));
+		CartBean userCart = (CartBean) session.getAttribute("cart");
+		if (userCart == null) {
+			userCart = new CartBean();
+		}
+		ProductBeanDAO flightDAO = new ProductBeanDAO();
+		ProductBean flightBean = flightDAO.searchByID(flightId);
+		userCart.addProduct(flightBean);
+		if (!userCart.getUser().isEmpty()) {
+			userCart.saveCart(userCart, flightBean);
+		}
+		session.removeAttribute("cart");
+		session.setAttribute("cart", userCart);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
